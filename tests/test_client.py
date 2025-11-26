3a895871a4d91237fe99551a0d851afeaa82614d
Min RK <benjaminrk@gmail.com>
2025-11-25 17:04:57 -0800

    add and exercise a few more api endpoints
    - patient lookup by email
    - list data sources
    - filter patients by org and study
    - filter study by org

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,6 +2,9 @@ import os
 from importlib import reload
 from unittest import mock
 
+import pytest
+import requests
+
 import jupyterhealth_client
 from jupyterhealth_client import JupyterHealthClient
 
@@ -21,3 +24,79 @@ def test_client_constructor():
 
 
 # TODO: really test the client, but we need mock responses first
+def test_get_user(jh_client):
+    user = jh_client.get_user()
+    assert type(user) is dict
+    assert user["id"] == 10008
+
+
+def test_get_organization(jh_client):
+    org = jh_client.get_organization(20013)
+    assert org["id"] == 20013
+    assert org["name"] == "Department"
+    with pytest.raises(requests.HTTPError) as exc:
+        org = jh_client.get_organization(404)
+    assert exc.value.requests_error.response.status_code == 404
+
+
+def test_list_organizations(jh_client):
+    orgs = list(jh_client.list_organizations())
+    assert len(orgs) == 4
+    assert orgs[0]["id"] == 20013
+    assert orgs[0]["name"] == "Department"
+
+
+def test_get_study(jh_client):
+    study = jh_client.get_study(30001)
+    assert study["id"] == 30001
+    assert study["name"] == "ACME Health CGM Study"
+    assert study["organization"]["id"] == 20013
+    with pytest.raises(requests.HTTPError) as exc:
+        jh_client.get_study(404)
+    assert exc.value.requests_error.response.status_code == 404
+    str(exc.value)
+
+
+def test_list_studies(jh_client):
+    studies = list(jh_client.list_studies())
+    assert len(studies) == 2
+    assert studies[0]["id"] == 30001
+    assert studies[1]["id"] == 30012
+
+    studies = list(jh_client.list_studies(organization_id=20013))
+    assert len(studies) == 1
+    assert studies[0]["id"] == 30001
+
+    studies = list(jh_client.list_studies(organization_id=404))
+    assert len(studies) == 0
+
+
+def test_get_patient(jh_client):
+    patient = jh_client.get_patient(40001)
+    assert patient["id"] == 40001
+    with pytest.raises(requests.HTTPError) as exc:
+        jh_client.get_patient(404)
+
+    assert exc.value.requests_error.response.status_code == 404
+    # test exception rendering
+    strerror = str(exc.value)
+    assert "No patient matches" in strerror
+    assert "404" in strerror
+
+
+def test_lookup_patient(jh_client):
+    patient = jh_client.lookup_patient(email="demouser@jupyterhealth.example.org")
+    assert patient["id"] == 40001
+    with pytest.raises(KeyError):
+        patient = jh_client.lookup_patient(email="nosuchuser@jupyterhealth.example.org")
+    patient = jh_client.lookup_patient(external_id="demouser-min")
+    assert patient["id"] == 40001
+    with pytest.raises(KeyError):
+        patient = jh_client.lookup_patient(external_id="nomatch")
+    with pytest.raises(TypeError):
+        jh_client.lookup_patient()
+
+
+def test_list_data_sources(jh_client):
+    data_sources = list(jh_client.list_data_sources())
+    assert len(data_sources) == 2
