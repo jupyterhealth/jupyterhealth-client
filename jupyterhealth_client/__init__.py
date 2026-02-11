7175c5e840a3a8b323ec5eb1e329e7d82e40ce75
Min RK <benjaminrk@gmail.com>
2026-02-11 15:13:04 -0800

    Bump to 0.2.0

--- jupyterhealth_client/__init__.py
+++ jupyterhealth_client/__init__.py
@@ -2,7 +2,7 @@
 client library for JupyterHealth Exchange
 """
 
-__version__ = "0.1.0a3"
+__version__ = "0.2.0"
 
 from ._client import Code, JupyterHealthClient, RequestError
 from ._utils import tidy_observation
