93fed92955ab0cf590066e8d44004247f05bdf4d
Min RK <benjaminrk@gmail.com>
2026-07-22 16:04:55 -0700

    Bump to 0.4.0

--- jupyterhealth_client/__init__.py
+++ jupyterhealth_client/__init__.py
@@ -2,7 +2,7 @@
 client library for JupyterHealth Exchange
 """
 
-__version__ = "0.3.1.dev"
+__version__ = "0.4.0"
 
 from ._client import Code, JupyterHealthClient, RequestError
 from ._utils import tidy_observation
